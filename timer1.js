9fe34204c78b305be8cc86f336974d0f0674f12c
Marilynn Stone <hello@marilynnstone.com>
2022-06-14 13:27:38 +0000

    start to create time conversion and timer functions

--- timer1.js
+++ timer1.js
@@ -1,5 +1,6 @@
 // user enters comand line arguement to say when beeps will happen
 // create array using process.argv
+// convert time in seconds to milliseconds
 // loop though array
 // ignore edge cases
 // use setTimeout to space out the beeps
@@ -11,6 +12,26 @@
 // not a number => ignore/skip
 
 
-process.stdout.write('\x07');
+;
 
-const args = process.argv.slice(2);
+const timingArray = process.argv.slice(2);
+let convertedTime = [];
+
+const convertTime = function(timingArray) {
+  for (const time of timingArray) {
+    convertedTime.push(time * 1000);
+  }
+};
+
+const timer = function(timingArray) {
+  for (let num of timingArray) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, time);
+    time += timeIncrement;
+  }
+  // this adds a new line once spinnerText is completely printed
+  setTimeout(() => {
+    process.stdout.write('\n');
+  }, spinnerText.length * timeIncrement);
+};
