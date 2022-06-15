6916d3e07ebcf1272bc20e0bed2dc0e5e28e4914
Marilynn Stone <hello@marilynnstone.com>
2022-06-15 17:12:54 +0000

    add comments to explain functions

--- timer1.js
+++ timer1.js
@@ -1,20 +1,7 @@
-/* eslint-disable prettier/prettier */
-// user enters comand line arguement to say when beeps will happen
-// create array using process.argv
-// convert time in seconds to milliseconds
-// loop though array
-// ignore edge cases
-// use setTimeout to space out the beeps
-// add new line at end of function
-
-// edge cases:
-// no numbers => no  beep
-// negative number => ignore/skip
-// not a number => ignore/skip
-
+// create an array of numbers from the console input
 let timingArray = process.argv.slice(2).map(Number);
-console.log("timingArray:", timingArray);
 
+// convert numbers in array to milliseconds
 let convertedTime = [];
 const convertTime = function(times) {
   for (const time of times) {
@@ -24,8 +11,8 @@ const convertTime = function(times) {
   }
 };
 convertTime(timingArray);
-console.log("convertedTime:", convertedTime);
 
+// perform a system sound at each time
 const timer = function(convertedTimes) {
   for (let time of convertedTimes) {
     setTimeout(() => {
